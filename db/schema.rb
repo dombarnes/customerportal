3092c5fe1d9164cf47ba28904229ed7b0a74f5df
Dom Barnes <dom@dombarnes.com>
2013-10-27 21:42:06 +0000

    Switching to pg

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20131023103833) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "homes", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
